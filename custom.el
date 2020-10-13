a73673492f721ab2eff0a33d24495c36b01fda89
zrquan <zrquan.0218@gmail.com>
2020-10-13 20:09:03 +0800

    backup on 2020-10-13

---
+++ custom.el
@@ -0,0 +1,29 @@
+;; Do not write anything past this comment. This is where Emacs will
+;; auto-generate custom variable definitions.
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(evil-want-Y-yank-to-eol nil)
+ '(exec-path
+   '("c:/Program Files/Fortify/Fortify_SCA_and_Apps_20.1.1/bin" "C:/Program Files (x86)/Common Files/Oracle/Java/javapath" "C:/Windows/system32" "C:/Windows" "C:/Windows/System32/Wbem" "C:/Windows/System32/WindowsPowerShell/v1.0/" "C:/Windows/System32/OpenSSH/" "C:/Program Files (x86)/NVIDIA Corporation/PhysX/Common" "C:/Program Files/NVIDIA Corporation/NVIDIA NvDLISR" "C:/Program Files/Git/cmd" "C:/Program Files/dotnet/" "C:/Users/26299/AppData/Roaming/Python/Python38/Scripts" "C:/Program Files/Python/Python38/" "C:/Users/26299/AppData/Local/Microsoft/WindowsApps" "C:/Program Files/Microsoft VS Code/bin" "C:/Program Files/Bandizip/" "C:/Users/26299/.dotnet/tools" "C:/Program Files/Git/usr/bin" "C:/Program Files/Java/apache-maven-3.6.3/bin" "." "c:/Program Files/emacs/libexec/emacs/26.3/x86_64-w64-mingw32"))
+ '(graphviz-dot-dot-program "dot")
+ '(helm-mini-default-sources
+   '(helm-source-buffers-list helm-source-recentf helm-source-bookmarks helm-source-buffer-not-found))
+ '(neo-theme 'ascii)
+ '(org-agenda-files '("~/Documents/notes/GTD.org"))
+ '(org-bullets-bullet-list '("¶" "◉" "○"))
+ '(org-bullets-face-name nil)
+ '(org-plantuml-jar-path "~/.spacemacs.d/plantuml.jar")
+ '(package-selected-packages
+   '(ranger emoji-cheat-sheet-plus company-emoji web-beautify livid-mode skewer-mode simple-httpd json-mode json-snatcher json-reformat js2-refactor multiple-cursors js2-mode js-doc coffee-mode origami graphviz-dot-mode company-quickhelp yapfify pyvenv pytest pyenv-mode py-isort pip-requirements live-py-mode hy-mode dash-functional helm-pydoc cython-mode company-anaconda anaconda-mode pythonic youdao-dictionary names chinese-word-at-point unfill mwim flycheck-pos-tip pos-tip flycheck xterm-color shell-pop multi-term eshell-z eshell-prompt-extras esh-help smeargle orgit org-projectile org-category-capture org-present org-pomodoro alert log4e gntp org-mime org-download magit-gitflow magit-popup htmlize helm-gitignore helm-company helm-c-yasnippet gnuplot gitignore-mode gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link fuzzy evil-magit magit git-commit with-editor transient company-statistics company auto-yasnippet yasnippet ac-ispell auto-complete arc-dark-theme spaceline paradox hydra highlight-numbers helm-projectile projectile flx-ido evil-unimpaired f evil-search-highlight-persist evil-lisp-state ws-butler winum which-key volatile-highlights vi-tilde-fringe uuidgen use-package toc-org powerline restart-emacs request rainbow-delimiters pkg-info popwin persp-mode pcre2el spinner org-plus-contrib org-bullets open-junk-file neotree move-text macrostep lorem-ipsum linum-relative link-hint indent-guide lv hungry-delete hl-todo highlight-parentheses parent-mode highlight-indentation helm-themes helm-swoop epl helm-mode-manager helm-make helm-flx helm-descbinds helm-ag google-translate golden-ratio flx fill-column-indicator fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-tutor evil-surround highlight evil-numbers evil-nerd-commenter evil-mc evil-matchit smartparens evil-indent-plus evil-iedit-state iedit evil-exchange evil-escape evil-ediff evil-args evil-anzu anzu evil goto-chg undo-tree eval-sexp-fu elisp-slime-nav dumb-jump dash s diminish define-word column-enforce-mode clean-aindent-mode bind-map bind-key auto-highlight-symbol auto-compile packed aggressive-indent adaptive-wrap ace-window ace-link ace-jump-helm-line helm avy helm-core popup async))
+ '(paradox-github-token t)
+ '(plantuml-default-exec-mode 'jar)
+ '(plantuml-jar-path "~/.spacemacs.d/plantuml.jar"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(tooltip ((t (:background "#34323e" :foreground "#9a9aba" :underline nil :slant normal :weight normal)))))
--- init.el
+++ init.el
@@ -45,11 +45,13 @@ values."
      org
      markdown
      plantuml
+     (ranger :variables ranger-show-preview t)
      (shell :variables
-            shell-default-height 30
-            shell-default-position 'top)
+            shell-default-position 'top
+            shell-default-height 30)
      (syntax-checking :variables
                       syntax-checking-enable-by-default nil)
+     emoji
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
@@ -351,44 +353,36 @@ you should place your code here."
            (insert "\n")
            (python-indent-line)))))
   ;; org mode
+  (add-hook 'org-mode-hook 'turn-on-auto-fill)
   (use-package org
     :init
-    (turn-on-auto-fill)
+    (spacemacs/toggle-auto-fill-mode-on)
     :config
+    (setq org-hide-emphasis-markers t)
     (setq org-startup-indented t)
     (setq indent-tabs-mode nil)
     (setq default-tab-width 4))
+  (spacemacs/set-leader-keys-for-major-mode 'org-mode "n" 'org-toggle-narrow-to-subtree)
+  ;; org agenda
+  (setq org-agenda-custom-commands
+        '(
+          ("w" . "任务安排")
+          ("wa" "重要且紧急的任务" tags-todo "+PRIORITY=\"A\"")
+          ("wb" "重要但不紧急的任务" tags-todo "-weekly-monthly-daily+PRIORITY=\"B\"")
+          ("wc" "不重要且不紧急的任务" tags-todo "+PRIORITY=\"C\"")
+          ("W" "Weekly Review"
+           ((stuck "") ;; review stuck projects as designated by org-stuck-projects
+            (tags-todo "work")
+            (tags-todo "study")
+            (tags-todo "life")
+            (tags-todo "daily")
+            (tags-todo "weekly")
+            ))
+          ))
+
+  (setq org-todo-keywords
+        '((sequence "TODO(t)" "WAIT(w)" "SOMEDAY(s)" "|" "DONE(d!)" "CANCELED(c@/!)")))
   )
 
-;; Do not write anything past this comment. This is where Emacs will
-;; auto-generate custom variable definitions.
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(evil-want-Y-yank-to-eol nil)
- '(exec-path
-   (quote
-    ("c:/Program Files/Fortify/Fortify_SCA_and_Apps_20.1.1/bin" "C:/Program Files (x86)/Common Files/Oracle/Java/javapath" "C:/Windows/system32" "C:/Windows" "C:/Windows/System32/Wbem" "C:/Windows/System32/WindowsPowerShell/v1.0/" "C:/Windows/System32/OpenSSH/" "C:/Program Files (x86)/NVIDIA Corporation/PhysX/Common" "C:/Program Files/NVIDIA Corporation/NVIDIA NvDLISR" "C:/Program Files/Git/cmd" "C:/Program Files/dotnet/" "C:/Users/26299/AppData/Roaming/Python/Python38/Scripts" "C:/Program Files/Python/Python38/" "C:/Users/26299/AppData/Local/Microsoft/WindowsApps" "C:/Program Files/Microsoft VS Code/bin" "C:/Program Files/Bandizip/" "C:/Users/26299/.dotnet/tools" "C:/Program Files/Git/usr/bin" "C:/Program Files/Java/apache-maven-3.6.3/bin" "." "c:/Program Files/emacs/libexec/emacs/26.3/x86_64-w64-mingw32")))
- '(graphviz-dot-dot-program "dot")
- '(helm-mini-default-sources
-   (quote
-    (helm-source-buffers-list helm-source-recentf helm-source-bookmarks helm-source-buffer-not-found)))
- '(neo-theme (quote ascii))
- '(org-agenda-files (quote ("~/Documents/notes/GTD.org")))
- '(org-bullets-bullet-list (quote ("¶" "◉" "○")))
- '(org-bullets-face-name nil)
- '(org-plantuml-jar-path "~/.spacemacs.d/plantuml.jar")
- '(package-selected-packages
-   (quote
-    (origami graphviz-dot-mode company-quickhelp yapfify pyvenv pytest pyenv-mode py-isort pip-requirements live-py-mode hy-mode dash-functional helm-pydoc cython-mode company-anaconda anaconda-mode pythonic youdao-dictionary names chinese-word-at-point unfill mwim flycheck-pos-tip pos-tip flycheck xterm-color shell-pop multi-term eshell-z eshell-prompt-extras esh-help smeargle orgit org-projectile org-category-capture org-present org-pomodoro alert log4e gntp org-mime org-download magit-gitflow magit-popup htmlize helm-gitignore helm-company helm-c-yasnippet gnuplot gitignore-mode gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link fuzzy evil-magit magit git-commit with-editor transient company-statistics company auto-yasnippet yasnippet ac-ispell auto-complete arc-dark-theme spaceline paradox hydra highlight-numbers helm-projectile projectile flx-ido evil-unimpaired f evil-search-highlight-persist evil-lisp-state ws-butler winum which-key volatile-highlights vi-tilde-fringe uuidgen use-package toc-org powerline restart-emacs request rainbow-delimiters pkg-info popwin persp-mode pcre2el spinner org-plus-contrib org-bullets open-junk-file neotree move-text macrostep lorem-ipsum linum-relative link-hint indent-guide lv hungry-delete hl-todo highlight-parentheses parent-mode highlight-indentation helm-themes helm-swoop epl helm-mode-manager helm-make helm-flx helm-descbinds helm-ag google-translate golden-ratio flx fill-column-indicator fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-tutor evil-surround highlight evil-numbers evil-nerd-commenter evil-mc evil-matchit smartparens evil-indent-plus evil-iedit-state iedit evil-exchange evil-escape evil-ediff evil-args evil-anzu anzu evil goto-chg undo-tree eval-sexp-fu elisp-slime-nav dumb-jump dash s diminish define-word column-enforce-mode clean-aindent-mode bind-map bind-key auto-highlight-symbol auto-compile packed aggressive-indent adaptive-wrap ace-window ace-link ace-jump-helm-line helm avy helm-core popup async)))
- '(paradox-github-token t)
- '(plantuml-default-exec-mode (quote jar))
- '(plantuml-jar-path "~/.spacemacs.d/plantuml.jar"))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(tooltip ((t (:background "#34323e" :foreground "#9a9aba" :underline nil :slant normal :weight normal)))))
+(setq custom-file (expand-file-name "custom.el" dotspacemacs-directory))
+(load custom-file 'no-error 'no-message)
